f763012497f2f9aed394a89a04668a0e04b91460
adchau2k1 <adchau2k1@gmail.com>
2023-10-02 08:46:50 +0700

    add gg tag manager

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,6 +18,22 @@ const handleSearch = () => {
   openMobileSearch.value = false
   return navigateTo('/search')
 }
+
+onMounted(() => {
+  const script1 = document.createElement('script')
+  script1.async = true
+  script1.src = 'https://www.googletagmanager.com/gtag/js?id=G-QKQW0MBFH4'
+  document.head.appendChild(script1)
+
+  const script2 = document.createElement('script')
+  script2.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-QKQW0MBFH4');
+  `
+  document.head.appendChild(script2)
+})
 </script>
 
 <template>
